c97f102deebd574f5ef39112c5c2ce27db118893
Александр Юричук <alexandr.urichuk@teko.biz>
2018-07-10 11:13:39 +0300

    Добавил boost_program_options для парсинга аргументов программы.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include "lib.h"
+#include <boost/program_options.hpp>
 
 #include <iostream>
 
+namespace po = boost::program_options;
+
 int main(int argc, char* argv[])
 {
-    if (argc == 2) {
-        if (argv[1] == "--version") {
-            std::cout << lib::version();
-            return 0;
-        } else {
-            std::cout << "Unknown option. Use --version.";
-            return 1;
-        }
-    }
-    else if (argc == 0) {
-        auto iplist = lib::loadIpStream();
-        
+    po::options_description description("Allowed options");
+    description.add_options()
+        ("help", "produce help message")
+        ("version", "print program version")
+        ;
+
+    po::variables_map optionMap;
+    po::store(po::parse_command_line(argc, argv, description), optionMap);
+    po::notify(optionMap);    
+
+    if (optionMap.count("help")) {
+        std::cout << description << "\n";
+        return 1;
     }
 
+    if (optionMap.count("version")) {
+        std::cout << "Program version is: " << lib::version_major() << "." << lib::version_minor() << "." << lib::version_patch()  << std::endl;
+        return 1;
+    }  
+ 
     return 0;
 }
