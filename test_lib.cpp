2581d5d054f4cd476d7f049c370c7126dda5ce93
Александр Юричук <alexandr.urichuk@teko.biz>
2018-07-10 11:12:31 +0300

    Реализовал тесты на версию

--- test_lib.cpp
+++ test_lib.cpp
@@ -8,7 +8,9 @@ BOOST_AUTO_TEST_SUITE(test_suite_version)
 
 BOOST_AUTO_TEST_CASE(test_version_valid)
         {
-                BOOST_CHECK(lib::version() > 0);
+                BOOST_CHECK(lib::version_patch() > 0);
+                BOOST_CHECK(lib::version_major() > 0);
+                BOOST_CHECK(lib::version_minor() > 0);
         }
 
 BOOST_AUTO_TEST_SUITE_END()
