3dec4947e821783e7fa90dde7ba40a0bf5b8f6b2
alexb5 <alexb5@mail.ru>
2018-07-22 16:27:35 -0700

    Переделал под Gtest

--- test_lib.cpp
+++ test_lib.cpp
@@ -1,16 +1,10 @@
 #include "lib.h"
 
-#define BOOST_TEST_MODULE test_lib
+#include <gtest/gtest.h>
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_suite_version)
-
-BOOST_AUTO_TEST_CASE(test_version_valid)
-        {
-                BOOST_CHECK(lib::version_patch() > 0);
-                BOOST_CHECK(lib::version_major() > 0);
-                BOOST_CHECK(lib::version_minor() > 0);
-        }
-
-BOOST_AUTO_TEST_SUITE_END()
+TEST(version_test_case, version_test)
+{
+  EXPECT_GT(lib::version_patch(), 0);
+  EXPECT_GT(lib::version_major(), 0);
+  EXPECT_GT(lib::version_minor(), 0);
+}
